900fa821ef225f5c88a3c7f7af8410e214d54d18
Anirudh <icyph0x@protonmail.com>
2018-04-03 00:32:23 +0530

    ruby: init

---
+++ ruby/test.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/ruby -w
+
+# learn BEGIN
+BEGIN {
+	puts "initializing ruby"
+}
+
+puts "hello";
+
+# heredocs
+
+print <<EOF
+here docs are lame desu
+EOF
+
+# learn END
+
+END {
+	puts "end is here"
+}
+
+= begin
+block comments
+whoa
+= end
+
+# classes
+class Test
+	@@test = 0
+end
+
+test1 = Test.new
+# i think classes are stupid, gonna skip it
+
+# conditionals (yeah i know but whatever)
+if 1 > 2
+	puts "1 is greater than 2"
+elsif 1 == 2
+	puts "1 is equal to two"
+else
+	puts "1 is less than two"
+end
+
+$check = 1
+print "check is true" if $check # hmm, p cool
+
+unless 1 > 2  # can't grok very well :v
+	puts "kek"
+else
+	puts "topkek"
+end
+
+# cases
+lulz = 5
+case lulz
+when 0 .. 2
+	puts "asdf"
+when 3 .. 6
+	puts "lolz"
+else
+	puts "okae"
+end
