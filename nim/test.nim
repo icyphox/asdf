72458f37d9b41f4b8375f79a92a2f4c5a410a1d3
Anirudh <icyph0x@pm.me>
2018-05-23 21:31:10 +0530

    nim: add stuff

---
+++ nim/test.nim
@@ -0,0 +1,20 @@
+echo "What's your name? "
+const 
+  x = 1
+  y = 3
+  z = y + 5
+echo z
+
+if z == 10:
+  echo "nyet"
+else:
+  echo "OmegaLUL"
+
+let name = readLine(stdin)
+case name
+of "":
+  echo "don't have a name, sad!"
+of "lel":
+  echo "lol"
+else:
+  echo "KRAPpa"
