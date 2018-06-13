4425cf6269b3b572b4407c166df526a86330ddb2
Anirudh <icyph0x@pm.me>
2018-06-13 14:00:20 +0530

    nim: attempt writing a colors lib

--- nim/colors.nim
+++ nim/colors.nim
@@ -1,5 +1,15 @@
-import terminal
+import terminal, strformat
 
-setForegroundColor(fgBlue)
-setBackgroundColor(bgGreen)
-echo "blue?"
+proc blue(s: string): string =
+  setForegroundColor(fgBlue)
+  result = s
+
+proc green(s: string): string =
+  setForegroundColor(fgGreen)
+  result = s
+
+when isMainModule:
+  var greent = green("green")
+  echo blue("hey this is blue")
+  echo green("hey this is green")
+  echo fmt"formatting in {greent}"
