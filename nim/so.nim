3f3de5a7d8d4bb4fb86ca8286ee93d42a73a767c
Anirudh <icyph0x@pm.me>
2018-07-24 19:00:02 +0530

    nim: libnms procs werk

--- nim/so.nim
+++ nim/so.nim
@@ -1,3 +1,8 @@
 proc nms_exec(s: cstring): cstring {.importc, dynlib: "/usr/local/lib/libnms.so".}
+proc nms_set_auto_decrypt(i: cint) {.importc, dynlib: "/usr/local/lib/libnms.so".}
+proc nms_set_foreground_color(s: cstring) {.importc, dynlib: "/usr/local/lib/libnms.so".}
 
-echo nmsExec("lmao")
+
+nms_set_foreground_color("green")
+nms_set_auto_decrypt(1)
+echo nms_exec("auto decrypiting")
