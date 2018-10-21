4b36d1abe9215cc861cfbc2f3279c2c782172135
Anirudh <icyph0x@pm.me>
2018-10-21 10:21:24 +0530

    nim: opts finally!
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- nim/opts.nim
+++ nim/opts.nim
@@ -1,5 +1,13 @@
 import parseopt
 
-var p = initOptParser("lmao")
+proc writeHelp() =
+  echo "help"
 
-echo(p)
+for kind, key, val in getOpt():
+  case kind
+  of cmdArgument:
+    echo("hm")
+  of cmdLongOption, cmdShortOption:
+    case key
+    of "help", "h": writeHelp()
+  of cmdEnd: assert(false)
