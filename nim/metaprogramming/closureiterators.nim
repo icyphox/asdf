5452630499af52f4d93afe13963964370e8f0b77
Anirudh <icyph0x@pm.me>
2018-11-12 13:13:45 +0530

    nim(mp): closure iterators
    Signed-off-by: Anirudh <icyph0x@pm.me>

---
+++ nim/metaprogramming/closureiterators.nim
@@ -0,0 +1,18 @@
+import math
+
+proc powers(m: int): auto =
+  #return iterator: int {.closure.} = # Make a closure explicitly
+  return iterator: int = # Compiler makes this a closure for us
+    for n in 0..int.high:
+      yield n^m
+
+var
+  squares = powers(2)
+  cubes = powers(3)
+
+for i in 1..4:
+  echo "Square: ", squares() # 0, 1, 4, 9
+for i in 1..4:
+  echo "Cube: ", cubes()     # 0, 1, 8, 27
+echo "Square: ", squares()   # 16
+echo "Cube: ", cubes()       # 64
