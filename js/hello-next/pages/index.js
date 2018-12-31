122ce615e61286fcb312e79fa85f5c760540a062
Anirudh <icyph0x@pm.me>
2018-12-31 17:13:20 +0530

    js: some next.js bs
    Signed-off-by: Anirudh <icyph0x@pm.me>

---
+++ js/hello-next/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Header from '../components/Header'
+import Layout from '../components/Layout'
+
+const Index = () => (
+    <Layout>
+       <p>Hello Icy</p>
+    </Layout>
+)
+
+export default Index
