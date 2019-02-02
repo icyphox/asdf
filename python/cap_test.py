e2a0ddfd8fbaa9cd2df0950afcf5321e1f46c091
Anirudh <icyph0x@pm.me>
2019-02-02 09:59:53 +0530

    python: try out capstone
    Signed-off-by: Anirudh <icyph0x@pm.me>

---
+++ python/cap_test.py
@@ -0,0 +1,8 @@
+from capstone import *
+
+with open('./bin', 'rb') as f:
+    CODE = f.read()
+
+md = Cs(CS_ARCH_X86, CS_MODE_64)
+for i in md.disasm(CODE, 0x1000):
+    print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
